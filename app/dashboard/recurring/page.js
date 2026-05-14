caeb5725711e619765cd218bb6b773e91cae628d
project <yugesshravi2005@gmail.com>
2026-05-14 17:30:42 +0530

    chore(dev): clean duplicate routes and enable mobile development access

--- app/dashboard/recurring/page.js
+++
@@ -1,8 +0,0 @@
-'use client';
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = DashboardRecurringPage;
-var RecurringTransactionsPage_1 = require("@/src/components/recurring/RecurringTransactionsPage");
-function DashboardRecurringPage() {
-    return <RecurringTransactionsPage_1.RecurringTransactionsPage />;
-}
--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,11 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  allowedDevOrigins: [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'http://192.168.0.102:3000',
+  ],
 }
 
 export default nextConfig
