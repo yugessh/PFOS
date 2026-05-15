05d4d532f3dd3867d422d30a8b8b73fde4650984
project <yugesshravi2005@gmail.com>
2026-05-15 12:23:40 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,10 @@ const nextConfig = {
   allowedDevOrigins: [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
+    'http://[::1]:3000',
     'http://192.168.0.102:3000',
+    'http://192.168.0.102',
+    'http://0.0.0.0:3000',
   ],
 }
 
